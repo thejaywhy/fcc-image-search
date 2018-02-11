ddd20b04119794e3acac2e0e2dc18477be99670a
Glitch (jy-fcc-image-search) <none>
2018-02-11 19:23:53 +0000

    💁🚕 Updated with Glitch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var requests = require('request');
 
 // setup our datastore
 var datastore = require("./datastore").sync;
@@ -14,7 +15,62 @@ function initializeDatastoreOnProjectCreation() {
     } catch (err) {
       console.log("Init Error: " + err);
     }
+    console.log("connected:", dsConnected);
   }
+  
+}
+
+// Wrap the call to Google Custom Search Engine (CSE)
+// return the results
+// We're building our own instead of using something from NPM
+// because that's the whole point :)
+function cseSearch(referer, term, start) {
+  //#?key=INSERT_YOUR_API_KEY&cx=INSERT_YOUR_CSE_ID&q=QUERY&start=PAGE
+  var url = process.env.CSE_URL +
+      "?key=" + process.env.CSE_API_KEY +
+      "&cx=" + process.env.CX_ID +
+      "&q=" + term +
+      "&searchType=image";
+  //+ "&start=" + start;
+     
+  var options = {
+    url: url,
+    headers: {
+      'Referer': referer
+    },
+    json: true
+  };
+  
+  var results = [];
+  
+  requests(
+    options,
+    (err, res, body) => {
+    if (err) { 
+      results = err;
+    } else {
+      results = body.items;
+    }
+  });
+  
+  console.log(results);
+  console.log("testing");
+  return results;
+}
+
+// Transform the results from google CSE into
+// the output we want
+function transformResults(cseResults) {
+  if (cseResults == null) return [];
+  console.log(cseResults);
+  return cseResults.map(function(x) {
+    //console.log(x);
+    return {
+      image_url: x.link,
+      alt_text: x.title,
+      page_url: x.image.contextLink
+    };
+  });
 }
 
 // root, show welcome page / docs
@@ -25,17 +81,31 @@ app.get("/", function (request, response) {
 });
 
 // Build the Search Route
-app.get("/api/images", function (request, response) {
-  if (!request.body) return response.status(400).json({err: "missing body"});  
-
-  response.status(504).json({error: "not built yet"});
+app.get("/api/images/:term", function (request, response) { 
+  datastore.saveSearch({term: request.params.term});
+  var results, referer = null;
+  
+  // get the offset, if any
+  var offset = request.query.offset;
+  if (!offset) offset = 0;
+  
+  referer = request.protocol + "://" + request.hostname + request.originalUrl
+  
+  results = cseSearch(referer, request.params.term, offset);
+  console.log(results);
+  if ( results == null ) {
+    response.status(500).json({error: "Server Error"}); 
+  } else { 
+    //response.status(200).json(transformResults(results));
+    response.status(200).json({hello: "world"});
+  }
   
 });
 
 // Build the History Route
-app.get("/api/latest/images", function (request, response) {  
+app.get("/api/history/images", function (request, response) {  
    
-  response.status(504).json({error: "not built yet"});
+  response.status(200).json(datastore.recentSearches());
   
 });
 
